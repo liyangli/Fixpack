6486fa7b9c24c46b6bf522ba4e3718c479085a1a
liyangli <ylisoftware@163.com>
2013-09-10 10:45:10 +0800

    add pre btn

--- src/main/java/com/bohui/frames/BootomPanel.java
+++ src/main/java/com/bohui/frames/BootomPanel.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionListener;
  * Time: 下午2:56
  */
 public class BootomPanel extends JPanel {
+    public static Integer flag = 1;
     public JButton getNextBtn() {
         return nextBtn;
     }
@@ -55,33 +56,20 @@ public class BootomPanel extends JPanel {
 
         nextBtn.addActionListener(new NextListener());
         resetBtn.addActionListener(new ResetListener());
+        preBtn.addActionListener(new PreListener());
     }
 
-}
 
-/**
- * nextBtn listner
- */
-class NextListener implements ActionListener{
 
-    private Integer flag = 1;
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        //set preBtn  netBtn and leftPanel and rightPanel change
-        JButton nextBtn = (JButton)e.getSource();
-        BootomPanel bootom = (BootomPanel)nextBtn.getParent();
-        flag += 1;
-        //opeator left
-        setLeftPanelByNext(bootom);
-        //opeator right
-        RightPanel rightPanel = PluginMainFrame.rightPanels.get(flag);
+    public static void opeatorRight(BootomPanel bootom) {
+        RightPanel rightPanel = PluginMainFrame.rightPanels.get(BootomPanel.flag);
         PluginMainFrame frame = (PluginMainFrame)bootom.getParent().getParent().getParent().getParent();
-       // RightPanel panel = frame.getRightPanel();
+        // RightPanel panel = frame.getRightPanel();
         Component[] components = frame.getContentPane().getComponents();
         RightPanel panel = null;
         for(Component component:components){
             if(component instanceof RightPanel){
-                 panel = (RightPanel)component;
+                panel = (RightPanel)component;
                 break;
             }
         }
@@ -91,6 +79,27 @@ class NextListener implements ActionListener{
         frame.getContentPane().repaint();
         frame.invalidate();
         frame.validate();
+    }
+
+}
+
+/**
+ * preBtn click listener
+ */
+class PreListener implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+         //go back pre
+         JButton preBtn = (JButton)e.getSource();
+        BootomPanel bootom = (BootomPanel)preBtn.getParent();
+        setLeftPanelByPre(bootom);
+        BootomPanel.flag = BootomPanel.flag - 1;
+         if(BootomPanel.flag == 1){
+             preBtn.setVisible(false);
+         }
+
+        //opeator right
+        BootomPanel.opeatorRight(bootom);
 
     }
 
@@ -98,23 +107,66 @@ class NextListener implements ActionListener{
      * base nextBtn
      * @param bootom bootom Panel
      */
+    private void setLeftPanelByPre(BootomPanel bootom) {
+        if(BootomPanel.flag == 1){
+            JLabel firstLabel = PluginMainFrame.leftLables.get(BootomPanel.flag);
+            ImageIcon image =new ImageIcon(FixPackFrame.class.getClass().getResource("/").getPath() +"run.jpg");
+            firstLabel.setIcon(image);
+
+        }else{
+
+            JLabel label = PluginMainFrame.leftLables.get(BootomPanel.flag);
+            label.setIcon(null);
+
+            JLabel preLabel =  PluginMainFrame.leftLables.get(BootomPanel.flag-1);
+            ImageIcon runImage =new ImageIcon(FixPackFrame.class.getClass().getResource("/").getPath() +"run.jpg");
+            preLabel.setIcon(runImage);
+
+        }
+    }
+}
+
+/**
+ * nextBtn listner
+ */
+class NextListener implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //set preBtn  netBtn and leftPanel and rightPanel change
+        JButton nextBtn = (JButton)e.getSource();
+        BootomPanel bootom = (BootomPanel)nextBtn.getParent();
+        BootomPanel.flag += 1;
+        //opeator left
+        setLeftPanelByNext(bootom);
+        //opeator right
+        BootomPanel.opeatorRight(bootom);
+
+    }
+
+
+
+    /**
+     * base nextBtn
+     * @param bootom bootom Panel
+     */
     private void setLeftPanelByNext(BootomPanel bootom) {
-        if(flag == 1){
-            JLabel firstLabel = PluginMainFrame.leftLables.get(flag);
+        if(BootomPanel.flag == 1){
+            JLabel firstLabel = PluginMainFrame.leftLables.get(BootomPanel.flag);
             ImageIcon image =new ImageIcon(FixPackFrame.class.getClass().getResource("/").getPath() +"success.jpg");
             firstLabel.setIcon(image);
             bootom.getPreBtn().setVisible(false);
         }else{
             bootom.getPreBtn().setVisible(true);
-            JLabel label = PluginMainFrame.leftLables.get(flag);
+            JLabel label = PluginMainFrame.leftLables.get(BootomPanel.flag);
             ImageIcon image =new ImageIcon(FixPackFrame.class.getClass().getResource("/").getPath() +"run.jpg");
             label.setIcon(image);
 
-            JLabel preLabel =  PluginMainFrame.leftLables.get(flag-1);
+            JLabel preLabel =  PluginMainFrame.leftLables.get(BootomPanel.flag-1);
             ImageIcon runImage =new ImageIcon(FixPackFrame.class.getClass().getResource("/").getPath() +"success.jpg");
             preLabel.setIcon(runImage);
 
-            if(flag == 9){
+            if(BootomPanel.flag == 9){
                 // add complate btn
                 bootom.getPreBtn().setVisible(false);
                 bootom.getNextBtn().setVisible(false);
@@ -123,6 +175,7 @@ class NextListener implements ActionListener{
 
         }
     }
+
 }
 
 /**
